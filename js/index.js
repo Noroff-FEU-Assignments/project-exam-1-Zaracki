14603cf0e413b7a029058a40bd830e76ec15c5e7
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-09-14 11:03:26 +0200

    Starts

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const apiUrl = "https://a33.no/wp-json/wp/v2/posts/70";
+
+const resultsContainer = document.querySelector(".container");
+
+const response = await fetch(apiUrl);
+      const json = await response.json();
+
+      resultsContainer.innerHTML = `<div>${json.content.rendered}</div>`
