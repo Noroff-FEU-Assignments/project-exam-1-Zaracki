ee3bcf3cc7fa50bc1eff2c615b80667cba121a9a
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-09-19 17:58:12 +0200

    Making carousel

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+const carousel = document.querySelector(".carousel");
+const firstImg = carousel.querySelectorAll(".carousel-img")[0];
+const arrowIcons = document.querySelectorAll(".wrapper i");
+
+let firstImgWidth = firstImg.clientWidth + 20;
+
+arrowIcons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    if(icon.id == "left") {
+      carousel.scrollLeft -= firstImgWidth;
+    } else {
+      carousel.scrollLeft += firstImgWidth;
+    }
+  })
+})
+
+//carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ const apiUrl = "https://a33.no/wp-json/wp/v2/posts/70";
 
 const resultsContainer = document.querySelector(".container");
 
+
+
 const response = await fetch(apiUrl);
       const json = await response.json();
 
       resultsContainer.innerHTML = `<div>${json.content.rendered}</div>`
+      
