7b211f12eea887cbbd8455dea19f20c71d62a2e2
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-09-25 09:04:15 +0200

    fix

---
+++ js/blogpost.js
@@ -0,0 +1,11 @@
+const apiUrl = "https://a33.no/wp-json/wp/v2/posts/142?_embed";
+
+const resultsContainer = document.querySelector(".container");
+
+const response = await fetch(apiUrl);
+      const json = await response.json();
+
+      resultsContainer.innerHTML = `<img src="${json._embedded["wp:featuredmedia"][0].source_url}">
+      <h1>${json.title.rendered}</h1>
+      <div class="content-rendered">${json.content.rendered}</div>`
+      ;
--- js/index.js
+++ js/index.js
@@ -1,11 +1,58 @@
-const apiUrl = "https://a33.no/wp-json/wp/v2/posts/142?_embed";
+const apiUrl = "https://a33.no/wp-json/wp/v2/posts?_embed";
 
-const resultsContainer = document.querySelector(".container");
+const carousel = document.querySelector(".carousel");
+const arrowIcons = document.querySelectorAll(".wrapper i");
 
-const response = await fetch(apiUrl);
-      const json = await response.json();
 
-      resultsContainer.innerHTML = `<img src="${json._embedded["wp:featuredmedia"][0].source_url}">
-      <h1>${json.title.rendered}</h1>
-      <div class="content-rendered">${json.content.rendered}</div>`
-      ;
+
+
+
+
+async function renderHTML() {
+  try {
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+
+    const posts = json.results;
+
+    for (let i = 1; i <= 9; i++) {
+      let currentJson = json[i];
+
+      carousel.innerHTML += `
+      <div class="carousel-img">
+      <a href="post.html"><img src="${currentJson._embedded["wp:featuredmedia"][0].source_url}"></a>
+      <div class="lowerleft-carousel">
+        <p>${currentJson.title.rendered}</p>
+      </div>
+      <div class="upperright-carousel">
+        <p>wall.alphacoders.com</p>
+      </div>`
+    }
+
+    const firstImg = carousel.querySelectorAll(".carousel-img")[0];
+    let firstImgWidth = firstImg.clientWidth + 20;
+
+    arrowIcons.forEach(icon => {
+      icon.addEventListener("click", () => {
+        if(icon.id == "left") {
+          carousel.scrollLeft -= firstImgWidth;
+        } else {
+          carousel.scrollLeft += firstImgWidth;
+        }
+      })
+    })
+    
+  }
+
+  catch(error) {
+
+  }
+
+
+}
+renderHTML()
+
+
+
+//carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+
